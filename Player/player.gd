e454ff09776f7483a1dc938437c581751da943ec
RJB04 <rjb_04@hotmail.com>
2025-07-19 17:21:50 +0100

    Leg Balances Torso!

--- Player/player.gd
+++ Player/player.gd
@@ -9,10 +9,9 @@ extends Node2D
 	"4": $"Right Leg Link",
 }
 
-# swap this for testing limb scene
+# Swap this for testing limb scene
 var current_limb_scene: PackedScene = preload("res://Player/Limbs/Leg_basic/leg_basic.tscn")
 var current_limb: Node = null
-
 
 func _ready() -> void:
 	current_limb = torso
@@ -30,17 +29,40 @@ func _attach_limb_to_slot(key: String) -> void:
 	var slot = sockets.get(key)
 	if not slot or slot.get_child_count() > 1:
 		return
-	##utter witchcraft do not touch
+
+	# 🧪 Utter witchcraft – do not touch
 	var limb = current_limb_scene.instantiate()
 	slot.add_child(limb)
 	limb.global_position = slot.global_position
-	var joint =  PinJoint2D.new()
+
+	var joint = PinJoint2D.new()
 	joint.node_a = slot.get_path()
 	joint.node_b = limb.get_node("Hip").get_path()
-	joint.global_position = slot.global_position 
+	joint.global_position = slot.global_position
 	joint.bias = 0.0
 	joint.angular_limit_enabled
 	joint.angular_limit_lower = 0
 	joint.angular_limit_upper = 180
 	get_parent().add_child(joint)
+
+	var ligament1 = DampedSpringJoint2D.new()
+	ligament1.node_a = slot.get_path()
+	ligament1.node_b = limb.get_node("Hip").get_path()
+	ligament1.global_position = slot.global_position
+	get_parent().add_child(ligament1)
+
 	current_limb = limb
+
+	var foot = limb.get_node_or_null("Foot")
+	if foot and foot.has_signal("grounded_state_changed"):
+		foot.connect("grounded_state_changed", Callable(self, "_on_foot_grounded_changed"))
+
+func _on_foot_grounded_changed(is_grounded: bool) -> void:
+	if is_grounded:
+		print("Foot touched the ground")
+		torso.mass = 5.0
+		torso.gravity_scale = -1.0
+	else:
+		print("Foot left the ground")
+		torso.mass = 7.0
+		torso.gravity_scale = 1.0
