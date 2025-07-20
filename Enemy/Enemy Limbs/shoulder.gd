33bda35e218f51c5d1f2a76036a456f03775cc96
Edt12 <104518243+Edt12@users.noreply.github.com>
2025-07-20 21:06:10 +0100

    Auto stash before merge of "master" and "origin/master"

--- Enemy/Enemy Limbs/shoulder.gd
+++ Enemy/Enemy Limbs/shoulder.gd
@@ -2,5 +2,7 @@ extends RigidBody2D
 
 
 func _physics_process(delta: float) -> void:
-	var velocity = linear_velocity
+	#need expected position calculated every frame and then compare that to where it is
+	pass
+	
 	
--- Player/player.gd
+++ Player/player.gd
@@ -12,7 +12,7 @@ extends Node2D
 # Swap this for testing limb scene
 var current_limb_scene: PackedScene = preload("res://Player/Limbs/Head_basic/Head_basic.tscn")
 var current_limb: Node = null
-
+var spriteTexture : Texture2D = preload("res://icon.svg")
 func _ready() -> void:
 	current_limb = torso
 
@@ -34,16 +34,40 @@ func _attach_limb_to_slot(key: String) -> void:
 	var limb = current_limb_scene.instantiate()
 	slot.add_child(limb)
 	limb.global_position = slot.global_position
+	#spawn first joint
+	var jointOne = PinJoint2D.new()
+	jointOne.node_a = slot.get_path()
+	jointOne.node_b = limb.get_child(0).get_path()
+	var spawnPositionJointOne:Vector2 = slot.global_position
+	spawnPositionJointOne.y = spawnPositionJointOne.y - 4#offset based on pins holding slot to player DONT TOUCH
+	jointOne.global_position =  spawnPositionJointOne
+	jointOne.bias = 0.0
+	jointOne.angular_limit_enabled
+	jointOne.angular_limit_lower = 0
+	jointOne.angular_limit_upper = 180
+	#spawn second joint
+	var jointTwo= PinJoint2D.new()
+	jointTwo.node_a = slot.get_path()
+	jointTwo.node_b = limb.get_child(0).get_path()
+	var spawnPositionJointTwo:Vector2 = slot.global_position
+	spawnPositionJointTwo.y = spawnPositionJointOne.y + 9#offset based on pins holding slot to player DONT TOUCH
+	jointTwo.global_position =  spawnPositionJointOne
+	jointTwo.bias = 0.0
+	jointTwo.angular_limit_enabled
+	jointTwo.angular_limit_lower = 0
+	jointTwo.angular_limit_upper = 180
+	
+	var spriteJointOne : Sprite2D = Sprite2D.new()
+	var spriteJointTwo : Sprite2D = Sprite2D.new()
+	
+	spriteJointOne.texture = spriteTexture
+	spriteJointTwo.texture = spriteTexture
+	
+	get_parent().add_child(jointOne)
+	get_parent().add_child(jointTwo)
+	get_parent().get_node("jointOne").add_child(spriteJointOne)
+	get_parent().get_node("jointTwo").add_child(spriteJointTwo)
 
-	var joint = PinJoint2D.new()
-	joint.node_a = slot.get_path()
-	joint.node_b = limb.get_child(0).get_path()
-	joint.global_position = slot.global_position
-	joint.bias = 0.0
-	joint.angular_limit_enabled
-	joint.angular_limit_lower = 0
-	joint.angular_limit_upper = 180
-	get_parent().add_child(joint)
 
 	var ligament1 = DampedSpringJoint2D.new()
 	ligament1.node_a = slot.get_path()
