636df3f055879b868ae0b268284f315fbecee6ca
RJB04 <rjb_04@hotmail.com>
2025-07-21 20:31:52 +0100

    Full body spawning temp, head adjustments

--- Player/player.gd
+++ Player/player.gd
@@ -1,7 +1,7 @@
 extends Node2D
 
 var stand_strength = 200
-var roll_strength = 60
+var roll_strength = 70
 
 
 @onready var torso: RigidBody2D = $Torso
@@ -14,16 +14,28 @@ var roll_strength = 60
 }
 
 # Swap this for testing limb scene
-var current_limb_scene: PackedScene = preload("res://Player/Limbs/Leg_basic/leg_basic.tscn")
+var current_limb_scene: PackedScene
+var Head: PackedScene = preload("res://Player/Limbs/Head_basic/Head_basic.tscn")
+var Arm: PackedScene = preload("res://Player/Limbs/Arm_basic/arm_basic.tscn")
+var Leg: PackedScene = preload("res://Player/Limbs/Leg_basic/leg_basic.tscn")
+
 var current_limb: Node = null
 
 func _ready() -> void:
 	current_limb = torso
 
 func _process(_delta: float) -> void:
+	if Input.is_action_just_pressed("5"):
+		current_limb_scene = Head
+	if Input.is_action_just_pressed("1") or Input.is_action_just_pressed("2"):
+		current_limb_scene = Arm
+	if Input.is_action_just_pressed("3") or Input.is_action_just_pressed("4"):
+		current_limb_scene = Leg
+		
 	for key in ["1", "2", "3", "4", "5"]:
 		if Input.is_action_just_pressed(key):
 			_attach_limb_to_slot(key)
+
 
 func _physics_process(delta: float) -> void:
 	if current_limb and current_limb.has_method("physics_update"):
@@ -56,10 +68,6 @@ func _attach_limb_to_slot(key: String) -> void:
 	joint.node_a = slot.get_path()
 	joint.node_b = limb.get_child(0).get_path()
 	joint.global_position = slot.global_position
-	joint.bias = 0.0
-	joint.angular_limit_enabled
-	joint.angular_limit_lower = 0
-	joint.angular_limit_upper = 180
 	get_parent().add_child(joint)
 
 
