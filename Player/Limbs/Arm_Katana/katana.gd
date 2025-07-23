324f06fb8fae4a90ac080f59bce7cb854157a7c2
RJB04 <rjb_04@hotmail.com>
2025-07-23 14:13:31 +0100

    Slight improvements to Katana physics and stabbing

--- Player/Limbs/Arm_Katana/katana.gd
+++ Player/Limbs/Arm_Katana/katana.gd
@@ -1,6 +1,7 @@
 extends RigidBody2D
 
 var move_speed = 8000
+var deadzone = 10.0
 
 
 
@@ -8,11 +9,9 @@ func _physics_process(delta: float) -> void:
 	var target_pos = get_global_mouse_position()
 	var to_target = target_pos - global_position
 	var distance = to_target.length()
-	var deadzone = 10.0  # Adjust this value as needed
 
 	if distance > deadzone:
 		var direction = to_target.normalized()
 		apply_central_force(direction * move_speed)
 	else:
-		# Optionally, you can dampen the velocity to stop the object smoothly
 		linear_velocity = linear_velocity * 0.8
--- Player/Limbs/Arm_Katana/katana_tip.gd
+++ Player/Limbs/Arm_Katana/katana_tip.gd
@@ -1,6 +1,7 @@
-extends Area2D
+extends RigidBody2D
 
-@onready var Katana = $".."
+@onready var Katana = $"../Katana"
+@onready var tip_detector = $"../Katana Tip Detector"
 
 var anchor: Vector2
 var hooked = false
@@ -9,8 +10,8 @@ var tips_in = false
 var Katana_pos: Vector2
 
 func _ready():
-	connect("body_entered", Callable(self, "_on_body_entered"))
-	connect("body_exited", Callable(self, "_on_body_exited"))
+	tip_detector.connect("body_entered", _on_body_entered)
+	tip_detector.connect("body_exited", _on_body_exited)
 
 func _process(delta: float) -> void:
 	if not hooked:
@@ -19,20 +20,27 @@ func _process(delta: float) -> void:
 			hooked = true
 			Katana.lock_rotation = true
 			Katana_pos = Katana.global_position
+			freeze = true
+			
 		elif Input.is_action_pressed("right_mouse"):
 			Katana.lock_rotation = true
 		else:
 			Katana.lock_rotation = false
-	else:
-		if Input.is_action_just_released("left_mouse"):
+	elif Input.is_action_just_released("left_mouse"):
 			hooked = false
+			freeze = false
+	
 
 func _physics_process(delta: float) -> void:
 	if hooked:
+		print("hooked")
 		global_position = anchor
 		Katana.global_position = Katana_pos
+
 func _on_body_entered(body: Node) -> void:
-	tips_in = true
+	if body.is_in_group("Ground"):
+		tips_in = true
 
 func _on_body_exited(body: Node) -> void:
-	tips_in = false
+	if body.is_in_group("Ground"):
+		tips_in = false
