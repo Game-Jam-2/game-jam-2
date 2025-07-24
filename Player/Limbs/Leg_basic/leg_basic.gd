a07004d22bfa910e134cb1df3664dd9d2d91cc5c
RJB04 <rjb_04@hotmail.com>
2025-07-24 15:39:41 +0100

    leg to torso  muscles implemented

--- Player/Limbs/Leg_basic/leg_basic.gd
+++ Player/Limbs/Leg_basic/leg_basic.gd
@@ -3,6 +3,8 @@ extends Node2D
 @onready var KneeMuscle = $KneeMuscle
 @onready var AnkleMuscle = $AnkleMuscle
 @onready var Thigh = $Thigh
+@onready var current_connector: RigidBody2D = get_parent()
+@onready var Player: Node2D = current_connector.get_parent()
 
 var tilt_force := 1.0
 var max_tilt_speed := 100.0
@@ -13,6 +15,7 @@ var knee_rest_length: float
 var ankle_rest_length: float
 
 func _ready() -> void:
+	connect_torso_muscles()
 	knee_stiffness = KneeMuscle.stiffness
 	ankle_stiffness = AnkleMuscle.stiffness
 	knee_rest_length = KneeMuscle.rest_length
@@ -46,3 +49,46 @@ func _process(delta: float) -> void:
 		velocity.x = min(velocity.x + tilt_force, max_tilt_speed)
 
 	Thigh.linear_velocity = velocity
+
+func connect_torso_muscles():
+	var current_connector_num = int(String(current_connector.name).substr(10))
+	current_connector_num = int(current_connector_num)
+	print(current_connector_num)
+	
+	var connections = 0
+	var connector_count = 0
+	for child in Player.get_children():
+		if String(child.name).begins_with("Connector "):
+			connector_count += 1
+		
+		if connections == 2:
+			break
+		elif (int(String(child.name).substr(10)) == current_connector_num + 1):
+			connections += 1
+			var connector_joint_1 := DampedSpringJoint2D.new()
+			connector_joint_1.node_a = Thigh.get_path()
+			connector_joint_1.node_b = child.get_path()
+			connector_joint_1.length = Thigh.global_position.distance_to(child.global_position)
+			add_child(connector_joint_1)
+			
+			#break early if we know theres no connector lower
+			if current_connector_num == 1:
+				break
+			
+		elif (int(String(child.name).substr(10)) == current_connector_num - 1):
+			connections += 1
+			var connector_joint_2 := DampedSpringJoint2D.new()
+			connector_joint_2.node_a = Thigh.get_path()
+			connector_joint_2.node_b = child.get_path()
+			connector_joint_2.length = Thigh.global_position.distance_to(child.global_position)
+			add_child(connector_joint_2)
+			
+	if connections == 1:
+		var connect_to = "Connector " + String(connector_count)
+		for child in Player.get_children():
+			if child.name == connect_to:
+				var connector_joint_2 := DampedSpringJoint2D.new()
+				connector_joint_2.node_a = Thigh.get_path()
+				connector_joint_2.node_b = child.get_path()
+				connector_joint_2.length = Thigh.global_position.distance_to(child.global_position)
+				add_child(connector_joint_2)
