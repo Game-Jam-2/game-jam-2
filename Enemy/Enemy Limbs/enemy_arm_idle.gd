95722c9ee81a42dae9035ced6b0f8acd157f3d27
Edt12 <104518243+Edt12@users.noreply.github.com>
2025-07-24 18:17:12 +0100

    Enemy arm goes toward player
    Grabbing will be implemented later

---
+++ Enemy/Enemy Limbs/enemy_arm_idle.gd
@@ -0,0 +1,18 @@
+extends State
+var path_to_enemy_root_node : Node2D
+var raycasts: Array[RayCast2D]
+var latest_player_position: Vector2
+func enter(previous_state_path: String , data := {}):
+	path_to_enemy_root_node = object_reference.get_parent().get_parent().get_node("Torso")
+	for child in path_to_enemy_root_node.get_children():
+		if child is RayCast2D:
+			raycasts.append(child)
+
+
+func physics_update(_delta:float):
+	for raycast in raycasts:
+		if raycast.is_colliding() and raycast.get_collider().name == "Torso" :
+			latest_player_position = raycast.get_collision_point()
+			var data:Dictionary = {"player_position":latest_player_position,"raycasts":raycasts}
+			finished.emit("Enemy_Arm_Chasing",data)
+			
--- Enemy/limb_breaker.gd
+++ Enemy/limb_breaker.gd
@@ -2,5 +2,5 @@ extends RigidBody2D
 @export var tension_threshold: int 
 func _physics_process(delta: float) -> void:
 	var tension:Vector2 = linear_velocity - get_parent().get_node("Arm_Enemy").get_child(0).linear_velocity
-	if tension.length() > 1000:
+	if tension.length() > tension_threshold:
 		queue_free()
