b327a4b2b771fce89a8a7e366ae1ee6cfbffd271
RJB04 <rjb_04@hotmail.com>
2025-07-24 14:19:05 +0100

    Leg Improvements!

--- Player/Limbs/Leg_basic/leg_basic.gd
+++ Player/Limbs/Leg_basic/leg_basic.gd
@@ -7,24 +7,36 @@ extends Node2D
 var tilt_force := 1.0
 var max_tilt_speed := 100.0
 
+var knee_stiffness: float
+var ankle_stiffness: float
+var knee_rest_length: float
+var ankle_rest_length: float
+
+func _ready() -> void:
+	knee_stiffness = KneeMuscle.stiffness
+	ankle_stiffness = AnkleMuscle.stiffness
+	knee_rest_length = KneeMuscle.rest_length
+	ankle_rest_length = AnkleMuscle.rest_length
+
 func _process(delta: float) -> void:
 	
-	# Leg extension
 	if Input.is_action_pressed("w"):
-		KneeMuscle.stiffness = 500
-		AnkleMuscle.stiffness = 500
+		KneeMuscle.stiffness = 64
+		AnkleMuscle.stiffness = 64
 		KneeMuscle.rest_length = KneeMuscle.length
 		AnkleMuscle.rest_length = AnkleMuscle.length
 
-	if Input.is_action_just_released("w"):
-		KneeMuscle.stiffness = 64
-		AnkleMuscle.stiffness = 64
+	elif Input.is_action_just_released("w"):
+		KneeMuscle.stiffness = knee_stiffness
+		AnkleMuscle.stiffness = ankle_stiffness
 
-	# Leg contraction
-	if Input.is_action_pressed("s"):
+	elif Input.is_action_pressed("s"):
 		KneeMuscle.rest_length = 5
 		AnkleMuscle.rest_length = 5
-
+		await get_tree().create_timer(1).timeout
+		KneeMuscle.rest_length = knee_rest_length
+		AnkleMuscle.rest_length = ankle_rest_length
+	
 	# Tilt control with velocity clamping
 	var velocity = Thigh.linear_velocity
 
--- Player/player.gd
+++ Player/player.gd
@@ -15,8 +15,8 @@ var roll_strength = 75
 
 # Swap this for testing limb scene
 var current_limb_scene: PackedScene
-var Head: PackedScene = preload("res://Player/Limbs/Head_grappleTongue/Head_grappleTongue.tscn")
-var Arm: PackedScene = preload("res://Player/Limbs/Arm_Katana/arm_katana.tscn")
+var Head: PackedScene = preload("res://Player/Limbs/Head_basic/Head_basic.tscn")
+var Arm: PackedScene = preload("res://Player/Limbs/Arm_basic/arm_basic.tscn")
 var Leg: PackedScene = preload("res://Player/Limbs/Leg_basic/leg_basic.tscn")
 
 var current_limb: Node = null
