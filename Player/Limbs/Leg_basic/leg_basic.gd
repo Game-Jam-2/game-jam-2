56bfb746c96200d7aa749c372fc63de8688d626e
RJB04 <rjb_04@hotmail.com>
2025-07-25 18:20:43 +0100

    leg progress

--- Player/Limbs/Leg_basic/leg_basic.gd
+++ Player/Limbs/Leg_basic/leg_basic.gd
@@ -4,29 +4,44 @@ extends Node2D
 @onready var AnkleMuscle = $AnkleMuscle
 @onready var Thigh = $Thigh
 @onready var Foot = $Foot
+@onready var ground_detector: Area2D = $Foot/ground_detection
 @onready var current_connector: RigidBody2D = get_parent()
 @onready var Player: Node2D = current_connector.get_parent()
 @onready var Torso: RigidBody2D = Player.get_node("Torso")
 @onready var left_torso_muscle: DampedSpringJoint2D
 @onready var right_torso_muscle: DampedSpringJoint2D
 
-var tilt_force := 1.0
-var max_tilt_speed := 100.0
+var is_grounded: bool
+var torso_target: Vector2
+var stiff: float
 
 var knee_stiffness: float
 var ankle_stiffness: float
 var knee_rest_length: float
 var ankle_rest_length: float
 
+
 func _ready() -> void:
+	signal_setup()
 	connect_torso_muscles()
 	knee_stiffness = KneeMuscle.stiffness
 	ankle_stiffness = AnkleMuscle.stiffness
 	knee_rest_length = KneeMuscle.rest_length
 	ankle_rest_length = AnkleMuscle.rest_length
 
+func signal_setup():
+	ground_detector.connect("body_entered", _on_ground_entered)
+	ground_detector.connect("body_exited", _on_ground_exited)
+
+func _on_ground_entered(body):
+	if body.is_in_group("Ground"):
+		is_grounded = true
+
+func _on_ground_exited(body):
+	if body.is_in_group("Ground"):
+		is_grounded = false
+
 func _process(delta: float) -> void:
-	
 	if Input.is_action_pressed("w"):
 		KneeMuscle.stiffness = 64
 		AnkleMuscle.stiffness = 64
@@ -37,33 +52,29 @@ func _process(delta: float) -> void:
 		KneeMuscle.stiffness = knee_stiffness
 		AnkleMuscle.stiffness = ankle_stiffness
 
+
 	elif Input.is_action_pressed("s"):
 		KneeMuscle.rest_length = 5
 		AnkleMuscle.rest_length = 5
-		await get_tree().create_timer(1).timeout
+		torso_target.y += 100
+		
+	elif Input.is_action_just_released("s"):
 		KneeMuscle.rest_length = knee_rest_length
 		AnkleMuscle.rest_length = ankle_rest_length
+		torso_target.y -= 100
 
 	if Input.is_action_pressed("a"):
-		
-		pass
+		Torso.apply_central_force(Vector2(-50,0))
 	elif Input.is_action_pressed("d"):
-		
-		pass
+		Torso.apply_central_force(Vector2(50,0))
 
 func _physics_process(delta: float) -> void:
-	if is_torso_upright():
-		balance_torso_to_upright()
-	else:
-		stand_up()
+	find_torso_target()
+	balance()
 
-func is_torso_upright() -> bool:
-	var angle_deg = rad_to_deg(Torso.rotation)
-	return abs(angle_deg) < 45
 
 func connect_torso_muscles():
 	var current_connector_num = current_connector.name.substr(10).to_int()
-	print(current_connector_num)
 	
 	var connections = 0
 	var connector_count = 0
@@ -81,6 +92,7 @@ func connect_torso_muscles():
 			connector_joint_1.length = Thigh.global_position.distance_to(child.global_position)
 			add_child(connector_joint_1)
 			right_torso_muscle = connector_joint_1
+			stiff = right_torso_muscle.stiffness
 			
 		elif (int(String(child.name).substr(10)) == current_connector_num + 1):
 			connections += 1
@@ -116,58 +128,41 @@ func connect_torso_muscles():
 					right_torso_muscle = connector_joint_1
 					break
 
-func balance_torso_to_upright():
-	var torso_pos = Player.global_position
-	var foot_pos = Foot.global_position
-	var x_diff = torso_pos.x - foot_pos.x
-	var abs_diff = abs(x_diff)
+func find_torso_target():
+	var desired_height = 500
+	var target_x_offset = 50
+		
+	target_x_offset = ((Foot.global_position.x/abs(Foot.global_position.x)) * target_x_offset)
+		
+	var target_y = Foot.global_position.y - desired_height
+		
+	var target_x = Foot.global_position.x - target_x_offset
+		
+	torso_target = Vector2(target_x, target_y)
 
-	var balance_factor = clamp(abs_diff / 100.0, 0.0, 1.0)
-	var length_adjust = lerp(0.0, 10.0, balance_factor)
-	
-	if x_diff > 5:
-		# torso too far right
-		left_torso_muscle.rest_length = max(left_torso_muscle.rest_length - length_adjust, 5.0)
-		right_torso_muscle.rest_length = min(right_torso_muscle.rest_length + length_adjust, 100.0)
-	elif x_diff < -5:
-		# torso too far left
-		right_torso_muscle.rest_length = max(right_torso_muscle.rest_length - length_adjust, 5.0)
-		left_torso_muscle.rest_length = min(left_torso_muscle.rest_length + length_adjust, 100.0)
-
-	var target_stiffness = lerp(10.0, 100.0, balance_factor)
-	left_torso_muscle.stiffness = target_stiffness
-	right_torso_muscle.stiffness = target_stiffness
-	
-func stand_up():
+func balance():
+	if not is_grounded:
+		left_torso_muscle.stiffness = stiff
+		right_torso_muscle.stiffness = stiff
+		return
 	left_torso_muscle.stiffness = 0
 	right_torso_muscle.stiffness = 0
-	left_torso_muscle.damping = 0
-	right_torso_muscle.damping = 0
-	
-	var target_x = Player.global_position.x
-	var foot_x = Foot.global_position.x
-	var move_direction = sign(target_x - foot_x)
+	# Target velocity scaling (tune these)
+	var max_x_speed = 100.0
+	var max_y_speed = 75.0
+	var gain = 120.0  # responsiveness
 
-	KneeMuscle.stiffness = 60
-	AnkleMuscle.stiffness = 60
+	# Compute distance to target
+	var torso_dist = torso_target - Torso.global_position
 
-	var attempts := 0
-	while abs(Foot.global_position.x - target_x) > 5.0 and attempts < 30:
-		KneeMuscle.rest_length -= move_direction * 0.5
-		AnkleMuscle.rest_length += move_direction * 0.5
-		await get_tree().create_timer(0.05).timeout
-		attempts += 1
+	# Compute target velocity based on distance
+	var target_velocity = Vector2(
+		clamp(torso_dist.x * 0.2, -max_x_speed, max_x_speed),
+		clamp(torso_dist.y * 0.2, -max_y_speed, max_y_speed)
+	)
 
-	KneeMuscle.rest_length = knee_rest_length
-	AnkleMuscle.rest_length = ankle_rest_length
-	KneeMuscle.stiffness = 100
-	AnkleMuscle.stiffness = 100
+	# Calculate velocity difference
+	var velocity_diff = target_velocity - Torso.linear_velocity
 
-	Torso.apply_impulse(Vector2(0, -200))
-	Torso.apply_torque_impulse(-Torso.rotation * 500)
-	await get_tree().create_timer(0.4).timeout
-
-	left_torso_muscle.stiffness = 40
-	right_torso_muscle.stiffness = 40
-	left_torso_muscle.damping = 10
-	right_torso_muscle.damping = 10
+	# Apply central force proportional to velocity difference
+	Torso.apply_central_force(velocity_diff * Torso.mass * gain)
