4451d2960cadb454dc40be987f36c1822620edc1
r0ttenkiwifruit <keziah.l.davies43@gmail.com>
2025-07-28 15:30:57 +0100

    added cave bgs and tried to make meat grinder work

--- De Cave/meat_grinder.gd
+++ De Cave/meat_grinder.gd
@@ -1,12 +1,12 @@
-extends CollisionShape2D
+extends Area2D
 
 var limb = preload("res://De Cave/arm_cave_test.tscn")
-
 
 
 func _limb_sacrificed():
 	emit_signal("limb_sacrificed")
 
-func _on_meat_grinder_body_entered(body: Node2D) -> void:
+
+func _on_meat_grinder_area_entered(body: Node2D) -> void:
 	if body == limb:
 		_limb_sacrificed()
