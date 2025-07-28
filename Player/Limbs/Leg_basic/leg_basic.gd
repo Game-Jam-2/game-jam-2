761bbe8f007bb3b2a7b9551331b4c637eec51ff6
RJB04 <rjb_04@hotmail.com>
2025-07-28 16:21:22 +0100

    Kinda leg progress?

--- Player/Limbs/Leg_basic/leg_basic.gd
+++ Player/Limbs/Leg_basic/leg_basic.gd
@@ -15,6 +15,9 @@ extends Node2D
 var is_grounded: bool
 var torso_target: Vector2
 var stiff: float
+var hopping_direction:= Vector2.ZERO
+var is_hopping: bool = false
+var torso_inertia:int
 
 var knee_stiffness: float
 var ankle_stiffness: float
@@ -29,6 +32,7 @@ func _ready() -> void:
 	ankle_stiffness = AnkleMuscle.stiffness
 	knee_rest_length = KneeMuscle.rest_length
 	ankle_rest_length = AnkleMuscle.rest_length
+	torso_inertia = 8000
 
 func signal_setup():
 	ground_detector.connect("body_entered", _on_ground_entered)
@@ -49,20 +53,17 @@ func _process(delta: float) -> void:
 		KneeMuscle.rest_length = KneeMuscle.length
 		AnkleMuscle.rest_length = AnkleMuscle.length
 		if is_grounded:
-			#accumulates as long as grounded, adding force upward to begin jump, and once not grounded, release impulse
-			#this simulates bending leg to jump higher
-			pass
-
-	elif Input.is_action_just_released("w"):
-		KneeMuscle.stiffness = knee_stiffness
-		AnkleMuscle.stiffness = ankle_stiffness
-
+			var jump_force = Vector2(0, -Torso.mass * 1500.0)
+			Torso.apply_central_impulse(jump_force)
 
 	elif Input.is_action_pressed("s"):
-		print(KneeMuscle.rest_length)
-		print(AnkleMuscle.rest_length)
-		KneeMuscle.rest_length = 60
-		AnkleMuscle.rest_length = 30
+		KneeMuscle.stiffness = 64
+		AnkleMuscle.stiffness = 64
+		KneeMuscle.rest_length = KneeMuscle.length
+		AnkleMuscle.rest_length = AnkleMuscle.length
+		if is_grounded:
+			var fall_force = Vector2(0, -Torso.mass * 1500.0)
+			Torso.apply_central_impulse(fall_force)
 		
 	elif Input.is_action_just_released("s"):
 		KneeMuscle.rest_length = knee_rest_length
@@ -70,16 +71,17 @@ func _process(delta: float) -> void:
 		torso_target.y -= 100
 
 	if Input.is_action_pressed("a"):
-		Torso.apply_central_force(Vector2(-1000,20))
-		Foot.apply_central_force(Vector2(-20,20))
+		hopping_direction = Vector2.LEFT
 	elif Input.is_action_pressed("d"):
-		Torso.apply_central_force(Vector2(1000,200))
-		Foot.apply_central_force(Vector2(20,200))
+		hopping_direction = Vector2.RIGHT
+	else:
+		hopping_direction = Vector2.ZERO
 
 func _physics_process(delta: float) -> void:
 	find_torso_target()
 	limit_velocity()
 	balance()
+	hopping()
 
 func limit_velocity():
 	var max_speed = 300.0
@@ -165,7 +167,7 @@ func find_torso_target():
 	torso_target = Vector2(target_x, target_y)
 
 func balance():
-	if !is_grounded:
+	if !is_grounded and !is_hopping:
 		left_torso_muscle.stiffness = 64
 		right_torso_muscle.stiffness = 64
 		left_torso_muscle.bias = 0.9
@@ -200,3 +202,29 @@ func balance():
 
 	# Apply central force proportional to velocity difference
 	Torso.apply_central_force(velocity_diff * Torso.mass * gain)
+
+func hopping():
+	if hopping_direction == Vector2.ZERO:
+		KneeMuscle.rest_length = knee_rest_length
+		AnkleMuscle.rest_length = ankle_rest_length
+		Torso.inertia = torso_inertia
+		is_hopping = false
+		return
+	elif is_grounded: # and (abs(Foot.global_position.x) - abs(Torso.global_position.x) < 50 and abs(Foot.global_position.x) - abs(Torso.global_position.x) > -50):
+		is_hopping = true
+	
+	if is_hopping:
+		Torso.inertia = Torso.inertia*10
+		print("hopping")
+		KneeMuscle.rest_length = 40
+		AnkleMuscle.rest_length = 20
+		await get_tree().create_timer(0.2).timeout
+		
+		var hop_force = Vector2(hopping_direction.x * Torso.mass * 1000.0,0)
+		Torso.apply_central_impulse(hop_force)
+		await get_tree().create_timer(0.2).timeout
+		
+		KneeMuscle.rest_length = knee_rest_length
+		AnkleMuscle.rest_length = ankle_rest_length
+		is_hopping = false
+		Torso.inertia = Torso.inertia/10
