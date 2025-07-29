55eab0fa7fa175bc7dfb33610dd9aae510d004af
Edt12 <104518243+Edt12@users.noreply.github.com>
2025-07-29 22:32:58 +0100

    ARM IS FIXED LETS GOOOO

--- Player/Limbs/Arm_basic/grabbing.gd
+++ Player/Limbs/Arm_basic/grabbing.gd
@@ -3,7 +3,7 @@ extends State
 signal grabbing
 signal releasing
 
-var speed : int = 600
+var speed : int = 1000
 var swing_pin: PinJoint2D
 var sprite:Sprite2D
 var texture:Texture = load("res://icon.svg")
@@ -17,10 +17,13 @@ func enter(previous_state_path : String,data :={}) -> void:
 	hand = object_reference.get_parent().get_node("Hand")
 	bicep = object_reference.get_parent().get_node("Bicep")
 	forearm = object_reference.get_parent().get_node("ForeArm")
+	
+	
 	swing_pin = PinJoint2D.new()
-	swing_pin.set_node_a(object_reference.get_path())
+	swing_pin.set_node_a(hand.get_path())
 	swing_pin.set_node_b(data["object_collided"].get_path())
-	swing_pin.bias = 0.0
+	swing_pin.bias = 0.9
+	swing_pin.softness = 0.0
 	swing_pin.global_position = object_reference.get_parent().get_node("Grab Point").position
 	swing_pin.disable_collision = false
 	object_reference.get_parent().add_child(swing_pin)
